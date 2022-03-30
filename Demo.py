94f1e148258447aa441087f7db06e3ae67915206
komalsaluja <ks5908491@gmail.com>
2022-03-30 20:00:30 +0530

    Injecting dynamic content from view function to template file

---
+++ Demo.py
@@ -0,0 +1,9 @@
+import os
+print(__file__)
+print(os.path.abspath(__file__))
+print(os.path.relpath(__file__))
+print(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+print(TEMPLATE_DIR)
