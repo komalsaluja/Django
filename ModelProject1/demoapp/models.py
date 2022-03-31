1a3d7a3d094e769df2e0dead448a17fcf70c0ca2
komalsaluja <ks5908491@gmail.com>
2022-03-31 22:37:26 +0530

    Model Project 1 -> Employee Database

---
+++ ModelProject1/demoapp/models.py
@@ -0,0 +1,9 @@
+from pyexpat import model
+from django.db import models
+
+class Employee(models.Model):
+    eno=models.IntegerField()
+    ename=models.CharField(max_length=50)
+    esal=models.FloatField()
+    eaddr=models.CharField(max_length=50)
+
