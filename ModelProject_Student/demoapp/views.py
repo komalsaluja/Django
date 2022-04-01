a5d7dfc88b84b05059c2fc65584f5413ef97a0be
komalsaluja <ks5908491@gmail.com>
2022-04-02 00:40:40 +0530

    Added ORM, template, fakerModule to Model Project

--- ModelProject_Student/demoapp/views.py
+++ ModelProject_Student/demoapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from demoapp.models import Student
 
-# Create your views here.
+def stu_info(request):
+    student = Student.objects.all()
+    return render(request,'demoapp/student.html',{'student':student})
+    
