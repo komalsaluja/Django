1450d04c575e6867944ba173735bae5edb55e2cc
komalsaluja <ks5908491@gmail.com>
2022-04-01 20:09:14 +0530

    ORM in Model Project 1

--- ModelProject1/demoapp/views.py
+++ ModelProject1/demoapp/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from demoapp.models import Employee
 
-# Create your views here.
+def employee_info_view(request):
+    employee = Employee.objects.all()  #django-ORM
+    return render(request,"demoapp/emp.html",{'emp':employee})
